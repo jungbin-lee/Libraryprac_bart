521354ce69840aea9be5ca57a0be3819078570f8
jungbin-lee <hihihi486@naver.com>
2023-05-18 18:32:56 +0900

    get web img, permission

--- app/src/main/java/com/h2square/libraryprac_bart/MainActivity.kt
+++ app/src/main/java/com/h2square/libraryprac_bart/MainActivity.kt
@@ -3,6 +3,7 @@ package com.h2square.libraryprac_bart
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -15,5 +16,8 @@ class MainActivity : AppCompatActivity() {
             val myIntent = Intent(this,ViewProfile::class.java)
             startActivity(myIntent)
         }
+
+    Glide.with(this).load("https://thumb.mtstarnews.com/06/2023/04/2023041517354946468_1.jpg/dims/optimize").into(lectureImage)
+
     }
 }
