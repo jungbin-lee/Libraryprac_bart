c1b98e50c7e13e76a89dc8bbc86c3b80d66a0d75
jungbin-lee <hihihi486@naver.com>
2023-05-18 17:22:57 +0900

    photozoom library prac

--- app/src/main/java/com/h2square/libraryprac_bart/MainActivity.kt
+++ app/src/main/java/com/h2square/libraryprac_bart/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.h2square.libraryprac_bart
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        profileImg.setOnClickListener{
+            val myIntent = Intent(this,ViewProfile::class.java)
+            startActivity(myIntent)
+        }
     }
 }
---
+++ app/src/main/java/com/h2square/libraryprac_bart/ViewProfile.kt
@@ -0,0 +1,13 @@
+package com.h2square.libraryprac_bart
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewProfile : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_profile)
+
+    }
+}
